e349cd5e34084b962aa7724b57d92db360175789
Raissa Coelho <raissacoelho.1995@gmail.com>
2024-03-13 21:03:11 -0300

    add MPI

--- trabalho_r/src/main.rs
+++ trabalho_r/src/main.rs
@@ -1,26 +1,25 @@
 use std::error::Error;
-use rayon::prelude::*;
 use std::fs::File;
 use std::path::{Path, PathBuf};
+use std::time::Instant;
 use csv::{Reader, WriterBuilder};
-use std::time::{Instant};
-
+use mpi::traits::*;
+use rayon::prelude::*;
 
 /* Processamento de arquivos CSV */
-/*Estratégia:
+/* Estratégia:
 MPI for inter-node communication (across different machines or processes)
 Rayon for intra-node parallelism (within a single machine or process) */
 
+fn merge_csv<P: AsRef<Path>>(file_path: P, file_path2: P, rank: i32) -> Result<(), Box<dyn Error>> {
+    let output_path = PathBuf::from(format!("merged_{}.csv", rank));
 
-fn merge_csv_file<P: AsRef<Path>>(file_path: P, file_path2: P) -> Result<(), Box<dyn Error>> {
-    let output_path = PathBuf::from("merged.csv");
-    
     // Arquivo 1
-    let file = File::open(file_path)?;
+    let file = File::open(&file_path)?;
     let mut rdr = Reader::from_reader(file);
 
     // Arquivo 2
-    let file2 = File::open(file_path2)?;
+    let file2 = File::open(&file_path2)?;
     let mut rdr2 = Reader::from_reader(file2);
 
     // Arquivo de output
@@ -34,8 +33,11 @@ fn merge_csv_file<P: AsRef<Path>>(file_path: P, file_path2: P) -> Result<(), Box
 
     // Escrever registros em paralelo
     let mut wtr = WriterBuilder::new().from_writer(output_file);
-    wtr.write_record(rdr.headers()?)?;
+    if rank == 0 {
+        wtr.write_record(rdr.headers()?)?;
+    }
 
+    // vetor em paralelo
     let all_records: Vec<_> = records1.into_par_iter().chain(records2.into_par_iter()).collect();
 
     all_records.into_iter().try_for_each(|record| -> Result<(), Box<dyn Error>> {
@@ -47,8 +49,8 @@ fn merge_csv_file<P: AsRef<Path>>(file_path: P, file_path2: P) -> Result<(), Box
 }
 
 // checa número de colunas
-fn checa_size<P: AsRef<Path>>(file1: P) -> Result<usize, Box<dyn Error>>{
-    // Arquivo 
+fn checa_size<P: AsRef<Path>>(file1: P) -> Result<usize, Box<dyn Error>> {
+    // Arquivo
     let file = File::open(file1)?;
     let mut rdr = Reader::from_reader(file);
 
@@ -58,27 +60,37 @@ fn checa_size<P: AsRef<Path>>(file1: P) -> Result<usize, Box<dyn Error>>{
 }
 
 fn main() {
- 
+    let universe = mpi::initialize().unwrap();
+    let world = universe.world();
+    let rank = world.rank();
+
     println!("Arquivos CSV");
-    
+
     // número de colunas de cada um
-    let a = checa_size("sample_integers.csv");
-    let b = checa_size("sample_integers2.csv");
-    
+    let a = checa_size("car_prices.csv").unwrap();
+    let b = checa_size("car_prices2.csv").unwrap();
+
     // checa se possuem o mesmo número de colunas
-    if a.unwrap() == b.unwrap() {
+    if a == b {
         let start = Instant::now();
-    
+
         println!("Juntando...");
-        if let Err(err) = merge_csv_file("sample_integers.csv", "sample_integers2.csv") {
+
+        if let Err(err) = merge_csv("car_prices.csv", "car_prices2.csv", rank) {
             eprintln!("Error: {}", err);
         }
- 
-        let fim = start.elapsed();
-        let elapsed_secs = fim.as_secs() as f64 + f64::from(fim.subsec_millis()) / 1000.0;
-        println!("Concluído em {:.3}s.", elapsed_secs);
-    }else{
-        println!("Arquivos CSV diferentes");
-    }
 
+        world.barrier();
+
+        if rank == 0 {
+            let fim = start.elapsed();
+            let elapsed_secs = fim.as_secs() as f64 + f64::from(fim.subsec_millis()) / 1000.0;
+            println!("Concluído em {:.3}s.", elapsed_secs);
+        }
+
+    } else {
+        if rank == 0 {
+            println!("Arquivos CSV diferentes");
+        }
+    }
 }
